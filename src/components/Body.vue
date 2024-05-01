18986ff2a4593e47ef88f0ef7e7d655ac0aba2d0
Marco2309 <trujas_2309@hotmail.com>
2024-05-01 08:10:27 -0600

    hover basico

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,3 +1,19 @@
 <template>
-  <div class="w-9/12 mx-auto"></div>
+  <div class="w-9/12 mx-auto">
+    <section>
+      <h2>How to do hover</h2>
+      <div class='flex'>
+        <div class='w-3/6'>
+          <pre>
+            class="something hover:changeOnHover"
+          </pre>
+        </div>
+        <div  class='w-3/6'>
+          <p class="rouded-10 p-10 bg-orange-600 hover:bg-orange-200">
+            Do hover on me
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
