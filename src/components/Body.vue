4782394c82feb04c90e0c5d88613fda2589d19db
Marco2309 <trujas_2309@hotmail.com>
2024-05-01 16:37:41 -0600

    2 secciones de tailwin

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,19 +1,28 @@
+<script setup>
+import SectionContainer from "./SectionContainer.vue";
+</script>
+
 <template>
   <div class="w-9/12 mx-auto">
-    <section>
-      <h2>How to do hover</h2>
-      <div class='flex'>
-        <div class='w-3/6'>
-          <pre>
-            class="something hover:changeOnHover"
-          </pre>
-        </div>
-        <div  class='w-3/6'>
-          <p class="rouded-10 p-10 bg-orange-600 hover:bg-orange-200">
-            Do hover on me
-          </p>
-        </div>
-      </div>
-    </section>
+    <SectionContainer title="How to do hover" :center="true">
+      <template #left>
+        <code> class="bg-orange-600 hover:bg-orange-200 </code>
+      </template>
+      <p class="p-10 bg-orange-600 hover:bg-orange-200">Do hover on me</p>
+    </SectionContainer>
+    <SectionContainer title="first, last, odd, and even" :center="true">
+      <template #left>
+        <pre> 
+class="first:pt-0 last:pb-0 odd:bg-orange-800"
+class="first:pt-0 last:pb-0 odd:bg-orange-800"
+class="first:pt-0 last:pb-0 odd:bg-orange-800"
+        </pre>
+      </template>
+      <ul class="border">
+        <li class="my-2 first:mt-0 last:mb-0 odd:bg-orange-800 grid place-content-center text-white">texto 1</li>
+        <li class="border-y border-black my-2 first:mt-0 last:mb-0 odd:bg-orange-800 grid place-content-center ">texto 2</li>
+        <li class="my-2 first:mt-0 last:mb-0 odd:bg-orange-800 grid place-content-center text-white">texto 3</li>
+      </ul>
+    </SectionContainer>
   </div>
 </template>
---
+++ src/components/SectionContainer.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({ title: String, center: Boolean });
+</script>
+
+<template>
+  <section class="rounded-lg bg-gray-100 p-10 mt-10">
+    <h2>{{ title }}</h2>
+    <div class="flex">
+      <div class="w-3/6" :class="{'grid place-content-center': center}">
+        <slot name="left"></slot>
+      </div>
+      <div class="w-3/6">
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scope>
+h2 {
+  font-weight: bolder;
+}
+</style>
